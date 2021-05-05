9a31be6ad95ae414a91a1e3459226653b7837515
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2021-05-05 02:14:43 -0500

    home page project structure set

---
+++ src/app/components/home-page/city-info/city-info.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CityInfoComponent } from './city-info.component';
+
+describe('CityInfoComponent', () => {
+  let component: CityInfoComponent;
+  let fixture: ComponentFixture<CityInfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CityInfoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CityInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/components/home-page/home-page.module.ts
+++ src/app/components/home-page/home-page.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CitiesComponent } from './cities/cities.component';
+import { CityInfoComponent } from './city-info/city-info.component';
+import { PostsComponent } from './posts/posts.component';
+import { HomePageComponent } from './home-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CitiesComponent,
+    CityInfoComponent,
+    PostsComponent,
+    HomePageComponent
+  ],
   imports: [
     CommonModule
   ]
